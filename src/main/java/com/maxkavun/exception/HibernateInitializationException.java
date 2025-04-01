98e324b2f02048525012030c5553048c2b98e8af
Max <kavun.onficial@gmail.com>
2025-04-01 13:22:00 +0200

    feat: implemented HibernateSessionFactoryException

--- src/main/java/com/maxkavun/exception/HibernateInitializationException.java
+++
@@ -1,7 +0,0 @@
-package com.maxkavun.exception;
-
-public class HibernateInitializationException extends RuntimeException {
-    public HibernateInitializationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
