ee66dc681158bc57f628eb3029a585c99d0476a1
Max <kavun.onficial@gmail.com>
2025-04-21 15:04:50 +0200

    feat: created CookieUtil class for  handling cookies

---
+++ src/main/java/com/maxkavun/util/CookieUtil.java
@@ -0,0 +1,26 @@
+package com.maxkavun.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class CookieUtil {
+    private static final  String DEFAULT_SESSION_COOKIE_NAME = "CUSTOM_SESSION_ID";
+
+    private CookieUtil() {}
+
+    public static String getSessionIdFromCookie(HttpServletRequest request , String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookieName.equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static String getSessionIdFromCookie(HttpServletRequest request) {
+        return getSessionIdFromCookie(request , DEFAULT_SESSION_COOKIE_NAME);
+    }
+}
