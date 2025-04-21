39fba2b8d908bceb5fe6ddf70255bff8061f20e7
Max <kavun.onficial@gmail.com>
2025-04-21 15:26:36 +0200

    feat: created CitySearchForm

---
+++ src/main/java/com/maxkavun/dto/CitySearchForm.java
@@ -0,0 +1,20 @@
+package com.maxkavun.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CitySearchForm {
+
+        @NotBlank(message = "City name is empty")
+        @Size(min = 2, max = 60, message = "The city name must be between 2 and 60 characters long")
+        String cityName;
+
+}
