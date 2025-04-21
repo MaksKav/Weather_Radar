ba992b27e2d8ccff09386c0b2a7a3da244bc1a51
Max <kavun.onficial@gmail.com>
2025-04-21 15:02:33 +0200

    fix: changed AbstractEntity for better handling of repositories

--- src/main/java/com/maxkavun/entity/AbstractEntity.java
+++ src/main/java/com/maxkavun/entity/AbstractEntity.java
@@ -1,17 +1,10 @@
 package com.maxkavun.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.io.Serializable;
 
 @MappedSuperclass
-@Getter
-@Setter
 public abstract class AbstractEntity<ID extends Serializable> implements Serializable {
-
-    @Id
-    @Column
-    private ID id;
+    public abstract ID getId();
+    public abstract void setId(ID id);
 }
