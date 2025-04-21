9a20495ede071acbdfedf1bba4867c851b6143ce
Max <kavun.onficial@gmail.com>
2025-04-21 20:14:35 +0200

    feat: add a getter for DEFAULT_SESSION_COOKIE_NAME field

--- src/main/java/com/maxkavun/util/CookieUtil.java
+++ src/main/java/com/maxkavun/util/CookieUtil.java
@@ -2,9 +2,12 @@ package com.maxkavun.util;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
 
 public class CookieUtil {
-    private static final  String DEFAULT_SESSION_COOKIE_NAME = "CUSTOM_SESSION_ID";
+
+    @Getter
+    private static final String DEFAULT_SESSION_COOKIE_NAME = "CUSTOM_SESSION_ID";
 
     private CookieUtil() {}
 
