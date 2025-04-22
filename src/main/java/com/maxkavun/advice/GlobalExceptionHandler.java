34a2b52208e3aee373dbc85373b184373a83f93a
Max <kavun.onficial@gmail.com>
2025-04-22 10:31:00 +0200

    feat: added exception handlers

--- src/main/java/com/maxkavun/advice/GlobalExceptionHandler.java
+++ src/main/java/com/maxkavun/advice/GlobalExceptionHandler.java
@@ -1,8 +1,9 @@
 package com.maxkavun.advice;
 
+import com.maxkavun.exception.AuthorizationServiceException;
 import com.maxkavun.exception.LocationServiceException;
+import com.maxkavun.exception.RegistrationServiceException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,11 +12,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(LocationServiceException.class)
-    public String handleLocationServiceException(LocationServiceException ex , Model model) {
+    public String handleLocationServiceException(LocationServiceException ex ) {
         log.error("Handled LocationServiceException: {}", ex.getMessage(), ex);
         return "error";
     }
 
+    @ExceptionHandler(RegistrationServiceException.class)
+    public String handleRegistrationServiceException(RegistrationServiceException ex ) {
+        log.error("Handled RegistrationServiceException: {}", ex.getMessage(), ex);
+        return "error";
+    }
 
+    @ExceptionHandler(AuthorizationServiceException.class)
+    public String handleAuthorizationServiceException(AuthorizationServiceException ex) {
+        log.error("Handled AuthorizationServiceException: {}", ex.getMessage(), ex);
+        return "error";
+    }
 
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex) {
+        log.error("Handled Exception: {}", ex.getMessage(), ex);
+        return "error";
+    }
 }
