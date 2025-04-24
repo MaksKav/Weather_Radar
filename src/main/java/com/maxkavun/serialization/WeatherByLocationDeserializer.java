6cab0c07ed09c6d12fe69777f941ab7af700dff0
Max <kavun.onficial@gmail.com>
2025-04-24 15:13:29 +0200

    style: remove extra empty line

--- src/main/java/com/maxkavun/serialization/WeatherByLocationDeserializer.java
+++ src/main/java/com/maxkavun/serialization/WeatherByLocationDeserializer.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.maxkavun.dto.LocationWithWeatherDto;
-
 import java.io.IOException;
 
 public class WeatherByLocationDeserializer extends JsonDeserializer<LocationWithWeatherDto> {
