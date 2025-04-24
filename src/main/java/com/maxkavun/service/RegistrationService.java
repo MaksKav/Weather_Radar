78c86c4d2afbc34963ea521d67643fa7d9636235
Max <kavun.onficial@gmail.com>
2025-04-24 14:47:19 +0200

    refactor: removed unusable imports

--- src/main/java/com/maxkavun/service/RegistrationService.java
+++ src/main/java/com/maxkavun/service/RegistrationService.java
@@ -9,13 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class RegistrationService {
 
     private final UserRepository userRepository;
-
 
     @Transactional
     public boolean saveUserIfNotExists(String username, String password) {
