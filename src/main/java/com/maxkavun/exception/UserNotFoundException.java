76b654c1948ba5f3cfe4b684a511016066e2aee1
Max <kavun.onficial@gmail.com>
2025-04-29 13:09:54 +0200

    refactor: merged UserNotFoundException.java and SessionNotFoundException.java to NotFoundException

--- src/main/java/com/maxkavun/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.maxkavun.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
