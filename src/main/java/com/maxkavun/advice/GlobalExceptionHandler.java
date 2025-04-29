eb43ea7da9217ad239a9da1fc6e10a709493cc5d
Max <kavun.onficial@gmail.com>
2025-04-29 13:13:21 +0200

    refactor: merged all handlers to one

--- src/main/java/com/maxkavun/advice/GlobalExceptionHandler.java
+++ src/main/java/com/maxkavun/advice/GlobalExceptionHandler.java
@@ -11,27 +11,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(LocationServiceException.class)
-    public String handleLocationServiceException(LocationServiceException ex ) {
-        log.error("Handled LocationServiceException: {}", ex.getMessage(), ex);
-        return "error";
-    }
-
-    @ExceptionHandler(RegistrationServiceException.class)
-    public String handleRegistrationServiceException(RegistrationServiceException ex ) {
-        log.error("Handled RegistrationServiceException: {}", ex.getMessage(), ex);
-        return "error";
-    }
-
-    @ExceptionHandler(AuthorizationServiceException.class)
-    public String handleAuthorizationServiceException(AuthorizationServiceException ex) {
-        log.error("Handled AuthorizationServiceException: {}", ex.getMessage(), ex);
-        return "error";
-    }
-
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler({
+            LocationServiceException.class,
+            RegistrationServiceException.class,
+            AuthorizationServiceException.class,
+            Exception.class
+    })
     public String handleException(Exception ex) {
-        log.error("Handled Exception: {}", ex.getMessage(), ex);
+        log.error("Handled {}: {}", ex.getClass().getSimpleName(), ex.getMessage());
         return "error";
     }
+
 }
